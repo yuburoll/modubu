0e8d06b8ba91e31a6e76ec04cb278f4b3d499220
yuburoll <yuburoll@gmail.com>
2025-11-22 04:43:54 +0900

    edited keymaps

--- firmware/qmk/modubu/keymaps/default/keymap.c
+++ firmware/qmk/modubu/keymaps/default/keymap.c
@@ -7,32 +7,62 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_semistag_16_3(
                 KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P, 
                 KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN, 
-        KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_B,           KC_N,           KC_M,           KC_COMM,        LT(2,KC_DOT),   RSFT_T(KC_SLSH), 
-                                                        LALT_T(KC_ESC), LCTL_T(KC_TAB), LT(1,KC_SPC),   LT(1,KC_ENT),   LT(3,KC_BSPC),  LGUI_T(KC_RALT)),
+        KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_B,           KC_N,           KC_M,           KC_COMM,        KC_DOT,         RSFT_T(KC_SLSH), 
+                                                        KC_LCTL,        KC_LALT,        KC_SPC,         KC_SPC,         TG(1),          LGUI_T(KC_RALT)),
     [1] = LAYOUT_semistag_16_3(
                 KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0, 
-                KC_CAPS,        KC_GRV,         KC_MINS,        KC_EQL,         KC_LBRC,                        KC_RBRC,        S(KC_QUOT),     KC_QUOT,        KC_BSLS,        KC_SCLN, 
-        KC_TRNS,        KC_RCTL,        KC_INS,         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_TRNS,        KC_TRNS,        KC_TRNS, 
-                                                        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS),
+                KC_GRV,         KC_MINS,        KC_EQL,         KC_QUOT,        KC_BSLS,                        KC_LBRC,        KC_RBRC,        KC_UP,          KC_UP,          KC_SCLN, 
+        KC_TRNS,        KC_RCTL,        KC_INS,         KC_CAPS,        KC_PGUP,        KC_PGDN,        KC_HOME,        KC_END,         KC_LEFT,          KC_DOWN,      KC_RGHT,        KC_RSFT, 
+                                                        KC_TRNS,        KC_TRNS,        KC_TRNS,        TO(3),          KC_TRNS,        KC_TRNS),
     [2] = LAYOUT_semistag_16_3(
-                KC_INS,         KC_HOME,        KC_UP,          KC_END,         KC_PGUP,                        MS_WHLU,        MS_WHLL,        MS_UP,          MS_WHLR,        KC_NO, 
-                KC_DEL,         KC_LEFT,        KC_DOWN,        KC_RGHT,        KC_PGDN,                        MS_WHLD,        MS_LEFT,        MS_DOWN,        MS_RGHT,        KC_TRNS, 
-        KC_TRNS,        C(KC_Z),        C(KC_X),        C(KC_C),        C(KC_V),        C(KC_Y),        KC_APP,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-                                                KC_TRNS,        KC_TRNS,        KC_TRNS,        MS_BTN2,        MS_BTN1,        MS_BTN3),
+                KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,                        MS_WHLU,        MS_WHLL,        MS_UP,          MS_WHLR,        KC_NO, 
+                KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,                        MS_WHLD,        MS_LEFT,        MS_DOWN,        MS_RGHT,        KC_TRNS, 
+        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_Y,           KC_APP,         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+                                                        KC_TRNS,        KC_TRNS,        KC_TRNS,        MS_BTN1,        MS_BTN2,        MS_BTN3),
     [3] = LAYOUT_semistag_16_3(
                 KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10, 
                 RM_NEXT,        RM_VALU,        RM_SATU,        RM_HUEU,        RM_TOGG,                        KC_PSCR,        KC_SCRL,        KC_BRK,         KC_F11,         KC_F12,  
         KC_TRNS,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          QK_BOOT,        QK_RBT,         KC_NO,          KC_NO,          KC_NO,          KC_RGUI,        KC_RSFT, 
-                                                        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS),
+                                                        KC_TRNS,        KC_TRNS,        KC_TRNS,        TO(0),          TO(0),          KC_TRNS),
+};
+
+enum combos {
+  OP_BSPC,
+  NUM_BSPC,
+  LSCLN_ENT,
+  NUM_ENT,
+  SPCTG_L3,
+  QW_ESC,
+  AS_TAB,
+  JKL_MOUSE,
+  LDR_MOUSE
+};
+
+const uint16_t PROGMEM op_combo[] = {KC_O, KC_P, COMBO_END};
+const uint16_t PROGMEM num90_combo[] = {KC_9, KC_0, COMBO_END};
+const uint16_t PROGMEM lscln_combo[] = {KC_L, KC_SCLN, COMBO_END};
+const uint16_t PROGMEM numscln_combo[] = {KC_UP, KC_SCLN, COMBO_END};
+const uint16_t PROGMEM spctg_combo[] = {KC_SPC, TG(1), COMBO_END};
+const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM as_combo[] = {KC_A, KC_S, COMBO_END};
+const uint16_t PROGMEM jkl_combo[] = {KC_J, KC_K, KC_L, COMBO_END};
+const uint16_t PROGMEM ldr_combo[] = {MS_LEFT, MS_DOWN, MS_RGHT, COMBO_END};
+
+combo_t key_combos[] = {
+  [OP_BSPC] = COMBO(op_combo, KC_BSPC),
+  [NUM_BSPC] = COMBO(num90_combo, KC_BSPC),
+  [LSCLN_ENT] = COMBO(lscln_combo, KC_ENT),
+  [NUM_ENT] = COMBO(numscln_combo, KC_ENT),
+  [SPCTG_L3] = COMBO(spctg_combo, TO(3)),
+  [QW_ESC] = COMBO(qw_combo, KC_ESC),
+  [AS_TAB] = COMBO(as_combo, KC_TAB),
+  [JKL_MOUSE] = COMBO(jkl_combo, TO(2)),
+  [LDR_MOUSE] = COMBO(ldr_combo, TO(0))
 };
 
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LT(2, KC_SCLN):
-            return true;
-        case LT(1,KC_ENT):
-            return true;
-        case LT(1, KC_SPC):
             return true;
         default:
             return false;
@@ -41,7 +71,7 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
 
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case RSFT_T(KC_BSPC):
+        case RSFT_T(KC_SLSH):
             return true;
         default:
             return false;
