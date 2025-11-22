dbd69c296e9b666b86594a3af44756656700e3b1
yuburoll <yuburoll@gmail.com>
2025-11-22 19:11:44 +0900

    optimize keymap again

--- firmware/qmk/modubu/keymaps/numpad/keymap.c
+++ firmware/qmk/modubu/keymaps/numpad/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P, 
                 KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN, 
         KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_B,           KC_N,           KC_M,           KC_COMM,        KC_DOT,         RSFT_T(KC_SLSH), 
-                                                        KC_LCTL,        KC_LALT,        KC_SPC,         LT(1,KC_SPC),   MO(3),          LGUI_T(KC_RALT)),
+                                                        KC_LCTL,        KC_LALT,        KC_SPC,         LT(1,KC_SPC),   LT(3,KC_RALT),  KC_LGUI),
     [1] = LAYOUT_semistag_16_3(
                 KC_NO,          KC_HOME,        KC_UP,          KC_END,         KC_PGUP,                        KC_LBRC,        KC_RBRC,        KC_7,           KC_8,           KC_9, 
                 KC_INS,         KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_PGDN,                        KC_MINS,        KC_EQL,         KC_4,           KC_5,           KC_6, 
@@ -76,7 +76,7 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LT(1,KC_SPC):
             return true;
-        case MO(3):
+        case LT(3,KC_RALT):
             return true;
         default:
             return false;
